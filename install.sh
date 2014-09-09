95ed56e86a784d0996bfd2bdecdcc2c8e91e74b8
Janne Heß <jannehess@gmail.com>
2014-09-09 18:29:18 +0000

    Update install script to 21st century
    Not really. But the 20th at least

--- install.sh
+++ install.sh
@@ -1,8 +1,31 @@
-#/bin/bash
+#/usr/bin/zsh
 
-BASEDIR=$(dirname $0)
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s $BASEDIR/zshrc $HOME/.zshrc
-ln -s $BASEDIR/gitconfig $HOME/.gitconfig
-ln -s $BASEDIR/path $HOME/.path
-ln -s $BASEDIR/vimrc $HOME/.vimrc
+set -e
+
+install_dotfiles () {
+	ln -s $BASEDIR/gitconfig $HOME/.gitconfig
+	ln -s $BASEDIR/vimrc $HOME/.vimrc
+	ln -s $BASEDIR/prezto $HOME/.zprezto
+
+	# Prezto
+	ln -s $BASEDIR/prezto/runcoms/zlogin $HOME/.zlogin
+	ln -s $BASEDIR/prezto/runcoms/zlogout $HOME/.zlogout
+	ln -s $BASEDIR/prezto/runcoms/zpreztorc $HOME/.zpreztorc
+	ln -s $BASEDIR/prezto/runcoms/zprofile $HOME/.zprofile
+	ln -s $BASEDIR/prezto/runcoms/zshenv $HOME/.zshenv
+	ln -s $BASEDIR/prezto/runcoms/zshrc $HOME/.zshrc
+}
+
+init_submodules () {
+	CURRENTDIR=`pwd`
+	cd $BASEDIR
+	git submodule init
+	git submodule update
+	cd $CURRENTDIR
+}
+
+install_dotfiles
+init_submodules
+
