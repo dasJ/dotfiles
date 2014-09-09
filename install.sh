2b3f5e2cd163591a03423e1e5e4610c55e43338d
Janne Heß <jannehess@gmail.com>
2014-09-09 17:18:42 +0000

    Rename all files for better ... It's cooler now

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname $0)
 
-ln -s $BASEDIR/.zshrc $HOME/.zshrc
-ln -s $BASEDIR/.gitconfig $HOME/.gitconfig
-ln -s $BASEDIR/.path $HOME/.path
-ln -s $BASEDIR/.vimrc $HOME/.vimrc
+ln -s $BASEDIR/zshrc $HOME/.zshrc
+ln -s $BASEDIR/gitconfig $HOME/.gitconfig
+ln -s $BASEDIR/path $HOME/.path
+ln -s $BASEDIR/vimrc $HOME/.vimrc
