aec8aad7630a30cffd78caae044b17d5e1fc1ac4
Janne Heß <jannehess@gmail.com>
2014-09-27 20:49:15 +0200

    No need to fork prezto anymore

--- install.sh
+++ install.sh
@@ -5,17 +5,19 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
 
 install_dotfiles () {
-	ln -s $BASEDIR/gitconfig $HOME/.gitconfig
-	ln -s $BASEDIR/vimrc $HOME/.vimrc
-	ln -s $BASEDIR/prezto $HOME/.zprezto
+	ln -s $BASEDIR/gitconfig $HOME/.gitconfig || true
+	ln -s $BASEDIR/vimrc $HOME/.vimrc || true
+	ln -s $BASEDIR/zprezto $HOME/.zprezto || true
 
 	# Prezto
-	ln -s $BASEDIR/zlogin $HOME/.zlogin
-	ln -s $BASEDIR/zlogout $HOME/.zlogout
-	ln -s $BASEDIR/zpreztorc $HOME/.zpreztorc
-	ln -s $BASEDIR/zprofile $HOME/.zprofile
-	ln -s $BASEDIR/zshenv $HOME/.zshenv
-	ln -s $BASEDIR/zshrc $HOME/.zshrc
+	ln -s $BASEDIR/zlogin $HOME/.zlogin || true
+	ln -s $BASEDIR/zlogout $HOME/.zlogout || true
+	ln -s $BASEDIR/zpreztorc $HOME/.zpreztorc || true
+	ln -s $BASEDIR/zprofile $HOME/.zprofile || true
+	ln -s $BASEDIR/zshenv $HOME/.zshenv || true
+	ln -s $BASEDIR/zshrc $HOME/.zshrc || true
+
+	ln -s $BASEDIR/prompt_janne_setup $BASEDIR/zprezto/modules/prompt/functions/prompt_janne_setup || true
 }
 
 init_submodules () {
