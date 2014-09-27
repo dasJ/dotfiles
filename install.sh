fba6e661467d3af5a6e5bf56cca311b5b4c231b3
Janne Heß <jannehess@gmail.com>
2014-09-27 20:55:46 +0200

    Improve install script with foreach

--- install.sh
+++ install.sh
@@ -2,22 +2,14 @@
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-set -e
 
 install_dotfiles () {
-	ln -s $BASEDIR/gitconfig $HOME/.gitconfig || true
-	ln -s $BASEDIR/vimrc $HOME/.vimrc || true
-	ln -s $BASEDIR/zprezto $HOME/.zprezto || true
 
-	# Prezto
-	ln -s $BASEDIR/zlogin $HOME/.zlogin || true
-	ln -s $BASEDIR/zlogout $HOME/.zlogout || true
-	ln -s $BASEDIR/zpreztorc $HOME/.zpreztorc || true
-	ln -s $BASEDIR/zprofile $HOME/.zprofile || true
-	ln -s $BASEDIR/zshenv $HOME/.zshenv || true
-	ln -s $BASEDIR/zshrc $HOME/.zshrc || true
+	ln -s $BASEDIR/prompt_janne_setup $BASEDIR/zprezto/modules/prompt/functions/prompt_janne_setup
 
-	ln -s $BASEDIR/prompt_janne_setup $BASEDIR/zprezto/modules/prompt/functions/prompt_janne_setup || true
+	for toLink in gitconfig vimrc zprezto zlogin zlogout zpreztorc zprofile zshenv zshrc; do
+		ln -s $BASEDIR/$toLink $HOME/.$toLink
+	done
 }
 
 init_submodules () {
