df927362f939049f828e78d47476e8607fc4d281
Janne Heß <jannehess@gmail.com>
2016-08-24 17:07:18 +0200

    Make installer more sane

--- install.sh
+++ install.sh
@@ -35,42 +35,38 @@ dependencies=(
 )
 
 
-# file|requirement|graphical?
-cmdfiles=(
-	"$HOME/.gemrc|zsh|no"
-	"$HOME/.gitconfig|git|no"
-	"$HOME/.zprezto|zsh|no"
-	"$HOME/.zlogin|zsh|no"
-	"$HOME/.zlogout|zsh|no"
-	"$HOME/.zpreztorc|zsh|no"
-	"$HOME/.zprofile|zsh|no"
-	"$HOME/.zshenv|zsh|no"
-	"$HOME/.zshrc|zsh|no"
-	"$HOME/.vimrc|vim|no"
-	"$HOME/.tmux.conf|tmux|no"
-	"$HOME/.Xresources|urxvt|yes"
-	"$HOME/.xsession|zsh|yes"
-	"$HOME/.config/htop/htoprc|htop|no"
-	"$HOME/.vim/bundle:vundle|vim|no"
-	"$HOME/.tmux/plugins:tpm|tmux|no"
-	"$HOME/.gtkrc-2.0|zsh|yes"
-	"$HOME/.config/gtk-3.0/settings.ini:gtkrc-3.0|zsh|yes"
-	"$HOME/.config/pacaur/config:pacaur|pacaur|no"
+# file|graphical?
+linkfiles=(
+	"$HOME/.gemrc|no"
+	"$HOME/.gitconfig|no"
+	"$HOME/.zprezto|no"
+	"$HOME/.zlogin|no"
+	"$HOME/.zlogout|no"
+	"$HOME/.zpreztorc|no"
+	"$HOME/.zprofile|no"
+	"$HOME/.zshenv|no"
+	"$HOME/.zshrc|no"
+	"$HOME/.vimrc|no"
+	"$HOME/.tmux.conf|no"
+	"$HOME/.Xresources|yes"
+	"$HOME/.xsession|yes"
+	"$HOME/.config/htop/htoprc|no"
+	"$HOME/.vim/bundle:vundle|no"
+	"$HOME/.tmux/plugins:tpm|no"
+	"$HOME/.gtkrc-2.0|yes"
+	"$HOME/.config/gtk-3.0/settings.ini:gtkrc-3.0|yes"
+	"$HOME/.config/pacaur/config:pacaur|no"
 )
 # dir|graphical?
 mkdirs=(
 	"$HOME/.vim/backup|no"
 	"$HOME/.vim/swap|no"
-	"$HOME/.tmux|no"
-	"$HOME/.config/htop|no"
-	"$HOME/.config/gtk-3.0|yes"
-	"$HOME/.config/pacaur|yes"
 )
 
 ###
 # Variables
 ###
-BASEDIR=$(readlink -f $(dirname $0))
+BASEDIR="$(readlink -f $(dirname $0))"
 
 ###
 # Functions
@@ -81,58 +77,63 @@ checkDependencies() {
 	local requiredMissing=0
 
 	for dependency in "${dependencies[@]}"; do
-		if [ -f $BASEDIR/nographical ]; then
-			if [ `echo $dependency | awk -F '|' '{ print $3 }'` == "yes" ]; then
+		# Parse this dependency
+		prettyname="`echo "${dependency}" | awk -F '|' '{ print $1 }'`"
+		isoptional="`echo "${dependency}" | awk -F '|' '{ print $2 }'`"
+		isgraphical="`echo "${dependency}" | awk -F '|' '{ print $3 }'`"
+		checktype="`echo "${dependency}" | awk -F '|' '{ print $4 }'`"
+		checkarg="`echo "${dependency}" | awk -F '|' '{ print $5 }'`"
+		# Ignore if not graphical
+		if [ -f "${BASEDIR}/nographical" ]; then
+			if [ "${isgraphical}" == 'yes' ]; then
 				continue
 			fi
 		fi
-		prettyname=`echo $dependency | awk -F '|' '{ print $1 }'`
-		test `echo $dependency | awk -F '|' '{ print $2 }'` == "yes"
-		optional=$?
-		if [ $optional -eq 0 ]; then
-			echo -ne ":: [ .... ] Checking for $prettyname"
-		else
-			echo -ne ":: [ .... ] Checking for $prettyname (optional)"
-		fi
+		# Print a nice message
+		msgsuffix=''
+		test "${isgraphical}" == 'yes' && msgsuffix="${msgsuffix} (graphical)"
+		test "${isoptional}" != 'yes' && msgsuffix="${msgsuffix} (optional)"
+		echo -ne ":: [ .... ] Checking for ${prettyname} ${msgsuffix}"
 		# Perform the actual check
 		okay=0
-		arg=`echo $dependency | awk -F '|' '{ print $5 }'`
-		if [ `echo $dependency | awk -F '|' '{ print $4 }'` == "command" ]; then
-			hash $arg 2>/dev/null
-			okay=$?
-		fi
-		if [ `echo $dependency | awk -F '|' '{ print $4 }'` == "file" ]; then
-			test -f "$arg"
-			okay=$?
-		fi
-		if [ `echo $dependency | awk -F '|' '{ print $4 }'` == "exec" ]; then
-			$arg &> /dev/null
-			okay=$?
-		fi
+		case "${checktype}" in
+			'command')
+				hash "${checkarg}" &>/dev/null
+				;;
+			'file')
+				test -f "${arg}"
+				;;
+			'exec')
+				${arg} &>/dev/null
+				okay=$?
+				;;
+			*)
+				okay=255
+		esac
 		# Process check result
-		if [ $okay -eq 0 ]; then
+		if [ "${okay}" -eq 0 ]; then
 			echo -ne "\r:: [ \e[00;32mokay\e[00m ]\v\r"
 		else
 			echo -ne "\r:: [ \e[00;31mfail\e[00m ]\v\r"
-			if [ $optional -eq 0 ]; then
+			if [ "${isoptional}" == 'yes' ]; then
 				requiredMissing=1
 			else
 				optionalMissing=1
 			fi
 		fi
 	done
-	# Output stuff
-	if [ $requiredMissing -eq 1 ]; then
+	# Output results
+	if [ "${requiredMissing}" -eq 1 ]; then
 		echo "Could not find all required depdendencies."
-		if [ $optionalMissing -eq 1 ]; then
+		if [ "${optionalMissing}" -eq 1 ]; then
 			echo "Some optional dependencies are missing as well."
 		fi
 		exit 1
 	fi
-	if [ $optionalMissing -eq 1 ]; then
+	if [ "${optionalMissing}" -eq 1 ]; then
 		while true; do
 			read -p "Not all optional dependencies were found. Continue anyway? [yn] " yn
-			case $yn in
+			case "${yn}" in
 				[Yy]* ) break ;;
 				[Nn]* ) exit 1 ;;
 			esac
@@ -141,66 +142,72 @@ checkDependencies() {
 }
 
 creategitconfig() {
-	if ! [ -f $BASEDIR/gitcustom ]; then
+	if ! [ -f "${BASEDIR}/gitcustom" ]; then
 		read -p "Please enter your name for the git config: " name
 		read -p "Please enter your mail for the git config: " email
-		echo -e "[user]\n\tname = ${name}\n\temail = ${email}\n" > $HOME/.dotfiles/gitcustom
+		echo -e "[user]\n\tname = ${name}\n\temail = ${email}\n" > "${BASEDIR}/gitcustom"
 	else
 		echo ":: git configuration already exists."
 	fi
 }
 
+creategraphicalconfig() {
+	if ! [ -f "${BASEDIR}/graphical" ] && ! [ -f "${BASEDIR}/nographical" ]; then
+		read -p "Installing in a graphical environment? [yN] " -n 1 reply
+		if [[ "${reply}" =~ ^[YyJj] ]]; then
+			touch "${BASEDIR}/graphical"
+		else
+			touch "${BASEDIR}/nographical"
+		fi
+	fi
+}
+
 makedirs() {
 	for mkdir in "${mkdirs[@]}"; do
-		if [ "`echo "$mkdir" | awk -F "|" '{print $2}'`" == "yes" ]; then
-			if [ -f $BASEDIR/graphical ]; then
-				mkdir -pv `echo "$mkdir" | awk -F "|" '{print $1}'`
+		path="`echo "${mkdir}" | awk -F '|' '{print $1}'`"
+		graphical="`echo "${mkdir}" | awk -F '|' '{print $2}'`"
+		if [ "${graphical}" == 'yes' ]; then
+			if [ -f "${BASEDIR}/graphical" ]; then
+				mkdir -pv "${path}"
 			fi
 		else
-			mkdir -pv `echo "$mkdir" | awk -F "|" '{print $1}'`
+			mkdir -pv "${path}"
 		fi
 	done
 }
 
 link() {
-	for file in "${cmdfiles[@]}"; do
-		if hash `echo "$file" | awk -F "|" '{print $2}'` 2>/dev/null; then
-			filename=`echo "$file" | awk -F "|" '{print $1}'`
-			linkfrom=`basename "$filename" | sed 's/^\.\(.*\)/\1/'`
-			if [[ "$filename" == *":"* ]]; then
-				linkfrom=`echo "$filename" | awk -F ":" '{print $2}'`
-				filename=`echo "$filename" | awk -F ":" '{print $1}'`
+	for file in "${linkfiles[@]}"; do
+		filename="`echo "${file}" | awk -F '|' '{print $1}'`"
+		linkfrom="`basename "${filename}" | sed 's/^\.\(.*\)/\1/'`"
+		# Allow files to be renamed on linking
+		if [[ "${filename}" == *":"* ]]; then
+			linkfrom="`echo "$filename" | awk -F ':' '{print $2}'`"
+			filename="`echo "$filename" | awk -F ':' '{print $1}'`"
+		fi
+		# Ensure directory exists
+		if [ "`echo "$file" | awk -F '|' '{print $2}'`" == 'yes' ]; then
+			if [ -f "${BASEDIR}/graphical" ]; then
+				mkdir -pv "`dirname "${filename}"`"
+				ln -svT "${BASEDIR}/${linkfrom}" "${filename}"
 			fi
-			if [ "`echo "$file" | awk -F "|" '{print $3}'`" == "yes" ]; then
-				if [ -f $BASEDIR/graphical ]; then
-					ln -svT $BASEDIR/$linkfrom $filename
-				fi
-			else
-				ln -svT $BASEDIR/$linkfrom $filename
-			fi
+		else
+			mkdir -pv "`dirname "${filename}"`"
+			ln -svT "${BASEDIR}/${linkfrom}" "${filename}"
 		fi
 	done
 }
 
 updaterepos() {
-	git --git-dir=$BASEDIR/.git --work-tree=$BASEDIR submodule update --init --recursive
-	git --git-dir=$BASEDIR/.git --work-tree=$BASEDIR submodule foreach git pull origin master
+	git "--git-dir=${BASEDIR}/.git" "--work-tree=${BASEDIR}" submodule update --init --recursive
+	git "--git-dir=${BASEDIR}/.git" "--work-tree=${BASEDIR}" submodule foreach git pull origin master
 }
 
-# Graphical question
-if ! [ -f $BASEDIR/graphical ] && ! [ -f $BASEDIR/nographical ]; then
-	read -p "Installing in a graphical environment? [yN] " -n 1 -r
-	echo
-if [[ "$REPLY" =~ ^[Yy] ]]; then
-		touch $BASEDIR/graphical
-	else
-		touch $BASEDIR/nographical
-	fi
-fi
-echo ":: Checking requirements..."
-checkDependencies
 echo ":: Asking for configuration..."
 creategitconfig
+creategraphicalconfig
+echo ":: Checking requirements..."
+checkDependencies
 echo ":: Creating empty directories..."
 makedirs
 echo ":: Updating submodules..."
