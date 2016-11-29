cc235a38bf2317e316d8319a1b146505f49104a3
Janne Heß <jannehess@gmail.com>
2016-11-29 10:10:24 +0100

    zsh: create own git aliases

---
+++ zsh/include/git-aliases.zsh
@@ -0,0 +1,28 @@
+# git add
+alias ga='git add'
+alias gaa='ga --all'
+alias gai='ga -i'
+
+# git commit
+alias gc='git commit -v'
+alias gc!='gc --amend'
+
+# git checkout
+alias gc='git checkout'
+
+# git branch
+alias gb='git branch'
+
+# git clone
+alias gcl='git clone --recursive'
+
+# git diff
+alias gd='git diff'
+alias gdc='git diff --cached'
+
+# git push/pull
+alias gl='git pull'
+alias gp='git push'
+
+# git status
+alias gst='git status'
