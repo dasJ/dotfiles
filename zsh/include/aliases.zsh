eeae6f297f40a4ab78c453766002b489eb35bc9c
Janne Heß <jannehess@gmail.com>
2017-06-16 23:08:53 +0200

    Get my IP addresses

--- zsh/include/aliases.zsh
+++ zsh/include/aliases.zsh
@@ -52,6 +52,7 @@ alias yay='echo "\\(^o^)/"'
 alias lsblk='lsblk -o NAME,MAJ:MIN,SIZE,TYPE,UUID,LABEL,FSTYPE,MOUNTPOINT'
 alias userctl='systemctl --user'
 alias :q='exit'
+alias privip="ip a | grep 'inet ' | awk -F' ' '{print $2}'"
 alias pubip='dig +short myip.opendns.com @resolver1.opendns.com'
 
 # Includes
