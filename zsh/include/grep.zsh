1a1f783270cfa5bd3843beee9a8370098466e5a4
Janne Heß <jannehess@gmail.com>
2017-06-16 23:01:54 +0200

    Fix grep and add grep flavours

--- zsh/include/grep.zsh
+++ zsh/include/grep.zsh
@@ -4,14 +4,16 @@
 
 GREP_OPTIONS=''
 # Enable grep colors
-if echo | grep --color=auto &>/dev/null; then
+if echo | grep --color=auto '' &>/dev/null; then
 	GREP_OPTIONS+=' --color=auto'
 fi
 # Do not grep VCS directories
-if echo | grep --exclude-dir=.cvs &>/dev/null; then
+if echo | grep --exclude-dir=.cvs '' &>/dev/null; then
 	GREP_OPTIONS+=' --exclude-dir={.bzr,.CVS,.git,.hg,.svn}'
-elif echo | grep --exclude=.cvs &>/dev/null; then
+elif echo | grep --exclude=.cvs '' &>/dev/null; then
 	GREP_OPTIONS+=' --exclude={.bzr,.CVS,.git,.hg,.svn}'
 fi
-alias grep="grep $GREP_OPTIONS"
+alias grep="grep${GREP_OPTIONS}"
+alias fgrep="fgrep${GREP_OPTIONS}"
+alias egrep="egrep${GREP_OPTIONS}"
 unset GREP_OPTIONS
