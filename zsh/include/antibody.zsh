628d292a2f62e74e5a1e069099302e90f81be069
Janne Heß <janne@hess.ooo>
2018-06-28 20:51:26 +0200

    zsh: Switch to system antibody

--- zsh/include/antibody.zsh
+++ zsh/include/antibody.zsh
@@ -3,7 +3,7 @@
 ###############
 
 export ANTIBODY_HOME="$zshconf/antibody/repos"
-source <($zshconf/../scripts/antibody init)
+source <(antibody init)
 antibody bundle < "$zshconf/antibody/plugins.txt"
 # Afterwards to enforce it's the last thing
 eval $(antibody bundle dasJ/zsh-theme)
