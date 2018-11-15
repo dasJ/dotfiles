3124b60a250196c923543c0061988ccb5b973bd4
Janne Heß <jannehess@gmail.com>
2018-11-15 18:01:49 +0100

    zsh: Fix agent path

--- zsh/include/ssh-agent.zsh
+++ zsh/include/ssh-agent.zsh
@@ -4,14 +4,14 @@
 
 if [ -z "${SSH_AUTH_SOCK}" ]; then
 	mkdir -p "${HOME}/.ssh"
-	export SSH_AUTH_SOCK="${HOME}/.ssh/.auth_socket"
+	export SSH_AUTH_SOCK="/tmp/${USER}/.auth_socket"
 fi
 
 if ! [ -S "${SSH_AUTH_SOCK}" ]; then
 	ssh-agent -a "${SSH_AUTH_SOCK}" > /dev/null
-	echo "${?}" > "${HOME}/.ssh/.auth_pid"
+	echo "${?}" > "/tmp/${USER}/.auth_pid"
 fi
 
-if [ -z "${SSH_AGENT_PID}" -a -f "${HOME}/.ssh/.auth_pid" ]; then
-	export SSH_AGENT_PID="$(< "${HOME}/.ssh/.auth_pid")"
+if [ -z "${SSH_AGENT_PID}" -a -f "/tmp/${USER}/.auth_pid" ]; then
+	export SSH_AGENT_PID="$(< "/tmp/${USER}/.auth_pid")"
 fi
