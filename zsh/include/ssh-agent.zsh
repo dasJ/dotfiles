11d256571bd1729939307a63adaa53183255f013
Janne Heß <janne@hess.ooo>
2020-03-24 23:55:13 +0100

    zsh/ssh: Redo the socket path

--- zsh/include/ssh-agent.zsh
+++ zsh/include/ssh-agent.zsh
@@ -3,15 +3,9 @@
 ###############
 
 if [ -z "${SSH_AUTH_SOCK}" ]; then
-	mkdir -p "${HOME}/.ssh"
-	export SSH_AUTH_SOCK="/tmp/${USER}/.auth_socket"
+	export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/.ssh_socket"
 fi
 
 if ! [ -S "${SSH_AUTH_SOCK}" ]; then
 	ssh-agent -a "${SSH_AUTH_SOCK}" > /dev/null
-	echo "${?}" > "/tmp/${USER}/.auth_pid"
-fi
-
-if [ -z "${SSH_AGENT_PID}" -a -f "/tmp/${USER}/.auth_pid" ]; then
-	export SSH_AGENT_PID="$(< "/tmp/${USER}/.auth_pid")"
 fi
