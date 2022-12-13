47e6c590b79fe3d8ab960eecab8500ca48e6cdf5
Janne Heß <janne@hess.ooo>
2022-12-13 09:48:52 +0100

    tmux-autoattach: Fix

--- zsh/include/tmux.zsh
+++ zsh/include/tmux.zsh
@@ -10,21 +10,15 @@ if hash tmux 2>/dev/null; then
 		export OUTERTMUX="${TMUX}"
 		unset TMUX
 	fi
-	# Check if we need to spawn the outer tmux
+	# Check if we already have an outer tmux
 	if [ -z "${OUTERTMUX}" ]; then
 		# Check if a base session exists
 		if [ "$(tmux -L "${tmuxname}" ls 2>/dev/null | grep "^${tmuxname}-base:" | wc -l)" = 0 ]; then
 			# Create new base session
 			exec tmux -2 -L "${tmuxname}" new-session -s "${tmuxname}-base"
 		else
-			# Kill old detached sessions
-			tmux -L "${tmuxname}" ls | \
-				grep '^autoattach-[[:digit:]]*:' | \
-				grep -v '(attached)$' | \
-				awk -F':' '{print $1}' | \
-				xargs -n1 tmux -L "${tmuxname}" kill-session -t
 			# Attach to existing base session
-			sessionid="$(date +%Y%m%d%H%M%S)"
+			sessionid="$(date +%s)"
 			tmux -2 -L "${tmuxname}" new-session -d -t "${tmuxname}-base" -s "${tmuxname}-${sessionid}"
 			exec tmux -2 -L "${tmuxname}" attach-session -t "${tmuxname}-${sessionid}"
 		fi
