febc583fb6c79c3b797bcea0f6421d279ae79507
Janne Heß <janne@hess.ooo>
2023-03-16 10:33:46 +0100

    ssh: Add FIDO2-aware askpass

--- zsh/include/ssh-agent.zsh
+++ zsh/include/ssh-agent.zsh
@@ -7,5 +7,8 @@ if [ -z "${SSH_AUTH_SOCK}" ]; then
 fi
 
 if ! [ -S "${SSH_AUTH_SOCK}" ]; then
+	if [[ -n "${DISPLAY}" ]] && hash dunstify 2>/dev/null; then
+		export SSH_ASKPASS="@DOTFILES@/scripts/fido2-askpass"
+	fi
 	ssh-agent -a "${SSH_AUTH_SOCK}" > /dev/null
 fi
