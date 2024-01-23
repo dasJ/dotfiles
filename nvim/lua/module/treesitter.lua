4a106d8095dbb6a31142b052e5d066c315b956e7
Janne Heß <janne@hess.ooo>
2024-01-23 13:15:17 +0100

    nvim: Add rust treesitter

--- nvim/lua/module/treesitter.lua
+++ nvim/lua/module/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-  ensure_installed = { "c", "bash", "nix", "lua" },
+  ensure_installed = { "c", "bash", "nix", "lua", "rust" },
   sync_install = false,
   highlight = { enable = true },
   indent = { enable = true }
