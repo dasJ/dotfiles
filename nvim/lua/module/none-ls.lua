126700261c215f11fd21c2281a72c3a7f6f42c60
Janne Heß <janne@hess.ooo>
2024-01-23 20:44:32 +0100

    nvim: Format on save in none-ls

--- nvim/lua/module/none-ls.lua
+++ nvim/lua/module/none-ls.lua
@@ -1,3 +1,4 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require("null-ls")
 
 null_ls.setup({
@@ -5,5 +6,20 @@ null_ls.setup({
     null_ls.builtins.code_actions.statix,
     null_ls.builtins.diagnostics.statix,
     null_ls.builtins.diagnostics.deadnix,
-  }
+  },
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                  -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                  -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                  vim.lsp.buf.formatting_sync()
+              end,
+          })
+      end
+  end,
 })
