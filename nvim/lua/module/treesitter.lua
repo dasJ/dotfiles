c9fa358935233b9dcc08fc1b09bcc5167297acef
Janne Heß <janne@hess.ooo>
2024-01-30 22:06:44 +0100

    nvim: Add PHP to treesitter

--- nvim/lua/module/treesitter.lua
+++ nvim/lua/module/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-  ensure_installed = { "c", "bash", "nix", "lua", "rust" },
+  ensure_installed = { "c", "bash", "nix", "lua", "php", "rust" },
   sync_install = false,
   highlight = { enable = true },
   indent = { enable = true }
