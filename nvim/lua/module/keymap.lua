fe7dc90537502415dc53aa5ee21aacf906fb2fee
Janne Heß <janne@hess.ooo>
2024-02-02 12:14:12 +0100

    nvim: Make `gf` create new files

--- nvim/lua/module/keymap.lua
+++ nvim/lua/module/keymap.lua
@@ -2,6 +2,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true })
+vim.keymap.set("n", "gf", ":e <cfile><cr>")
 
 vim.cmd("cnoreabbrev <expr> W ((getcmdtype() is# ':' && getcmdline() is# 'W')?('w'):('W'))")
 vim.cmd("cnoreabbrev <expr> Q ((getcmdtype() is# ':' && getcmdline() is# 'Q')?('q'):('Q'))")
