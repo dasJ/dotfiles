e79a5a478cdcd7c17a1355fcd642623f8e8ad93e
Janne Heß <janne@hess.ooo>
2024-03-06 11:13:02 +0100

    none-ls: Switch to treefmt

--- nvim/lua/module/none-ls.lua
+++ nvim/lua/module/none-ls.lua
@@ -6,6 +6,11 @@ null_ls.setup({
     null_ls.builtins.code_actions.statix,
     null_ls.builtins.diagnostics.statix,
     null_ls.builtins.diagnostics.deadnix,
+    null_ls.builtins.formatting.treefmt.with({
+      condition = function(utils)
+        return utils.root_has_file("treefmt.toml") or utils.root_has_file(".treefmt.toml")
+      end,
+    }),
   },
   -- you can reuse a shared lspconfig on_attach callback here
   on_attach = function(client, bufnr)
