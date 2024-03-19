3c60abbd1ed76a4043aabd959f5c3019a5768095
Janne Heß <janne@hess.ooo>
2024-03-19 11:21:54 +0100

    nvim: Fix gf for relative paths

--- nvim/lua/module/keymap.lua
+++ nvim/lua/module/keymap.lua
@@ -2,7 +2,14 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true })
-vim.keymap.set("n", "gf", ":e <cfile><cr>")
+vim.keymap.set("n", "gf", function()
+  cfile = vim.fn.expand('<cfile>')
+  if string.sub(cfile, 1, 2) == './' then
+    cfile = vim.fn.expand('%:p:h') .. '/' .. cfile
+  end
+
+  return ':e ' .. cfile .. '<cr>'
+end, { expr = true })
 
 vim.cmd("cnoreabbrev <expr> W ((getcmdtype() is# ':' && getcmdline() is# 'W')?('w'):('W'))")
 vim.cmd("cnoreabbrev <expr> Q ((getcmdtype() is# ':' && getcmdline() is# 'Q')?('q'):('Q'))")
