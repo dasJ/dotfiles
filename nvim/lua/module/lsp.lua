66d2cab2396fce7ce16c35713fae3c5a3f33f812
Janne Heß <janne@hess.ooo>
2025-11-26 09:44:42 +0100

    nvim: Multiple lsp fixups

--- nvim/lua/module/lsp.lua
+++ nvim/lua/module/lsp.lua
@@ -1,5 +1,3 @@
-local lspconfig = require('lspconfig')
-
 for _, server in ipairs {
   {
     "bashls",
@@ -41,12 +39,23 @@ for _, server in ipairs {
     },
   },
 } do
-  lspconfig[server[1]].setup {
+  vim.lsp.config(server[1], {
     cmd = server.cmd,
     on_attach = on_attach,
     capabilities = capabilities,
     settings = server.settings,
-  }
+  })
+  vim.lsp.enable(server[1])
+end
+
+for _, method in ipairs { "textDocument/diagnostic", "workspace/diagnostic" } do
+  local default_diagnostic_handler = vim.lsp.handlers[method]
+  vim.lsp.handlers[method] = function(err, result, context, config)
+    if err ~= nil and err.code == -32802 then
+      return
+    end
+    return default_diagnostic_handler(err, result, context, config)
+  end
 end
 
 -- Global mappings.
